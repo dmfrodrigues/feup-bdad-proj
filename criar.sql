935b1a374395062c165cdabc6546ca424064f4d5
Joao Sousa <joaosousa.pt@gmail.com>
2020-03-23 12:39:35 +0000

    Started criar.sql

---
+++ criar.sql
@@ -0,0 +1,23 @@
+
+
+-- TODO Check table declaration "order"
+DROP TABLE IF EXISTS ZipCode;
+DROP TABLE IF EXISTS Address_;      -- Address was highlighted but doesn't appear to be a reserved keyword
+DROP TABLE IF EXISTS PostalService;
+DROP TABLE IF EXISTS PostOffice;
+DROP TABLE IF EXISTS Vehicle;
+DROP TABLE IF EXISTS Person;
+DROP TABLE IF EXISTS Client;
+DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS Manager;
+DROP TABLE IF EXISTS ShopKeeper;
+DROP TABLE IF EXISTS Postman;
+DROP TABLE IF EXISTS Delivery;
+DROP TABLE IF EXISTS Order_;        -- Order is a reserved keyword
+DROP TABLE IF EXISTS Bill;
+DROP TABLE IF EXISTS CatalogItem;
+DROP TABLE IF EXISTS BillItem;
+DROP TABLE IF EXISTS Price;
+
+
+
