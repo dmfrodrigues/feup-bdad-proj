bb35b30500035dc39c31c90c78d60b528685c294
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-03-23 17:07:46 +0000

    changed criar.sql

--- criar.sql
+++ criar.sql
@@ -24,31 +24,71 @@ DROP TABLE IF EXISTS BillItem;
 DROP TABLE IF EXISTS Price;
 
 CREATE TABLE ZipCode (
-    country varchar( 2)         ,   -- ISO 3166-1 alpha-2 country code (PT for Portugal)
-    code    varchar(12)         ,   -- Postal code (alphanumeric and dashes)
-    town    varchar(63) NOT NULL,   -- Town name
-    postman varchar(15)         ,   -- Postman VAT number
-    CONSTRAINT PK_ZipCode PRIMARY KEY (country, code),
-    CONSTRAINT FK_ZipCode_Postman FOREIGN KEY (postman) REFERENCES Postman(vat)
+    country VARCHAR( 2)                         ,   -- ISO 3166-1 alpha-2 country code (PT for Portugal)
+    code    VARCHAR(12)                         ,   -- Postal code (alphanumeric and dashes)
+    town    VARCHAR(63) NOT NULL                ,   -- Town name
+    postman VARCHAR(15) REFERENCES Postman(vat) ,   -- Postman VAT number
+    PRIMARY KEY (country, code)
 );
 
 CREATE TABLE Address_ (             -- Address
-    idAddress       int                     ,
-    zipCode         varchar( 12)            ,   -- Postal code
-    country         varchar(  2)            ,   -- Country code
-    streetName      varchar(255) NOT NULL   ,   -- Street name
-    streetNumber    varchar( 15) NOT NULL   ,   -- Street number (main door number, may not have number: s/n)
-    doorNumber      varchar( 15)            ,   -- Door number if in an appartment block
-    personName      varchar(255)            ,   -- Addresser or addressee if applicable
-    CONSTRAINT PK_Address PRIMARY KEY (idAddress),
-    CONSTRAINT FK_Address_ZipCode FOREIGN KEY (zipCode, country) REFERENCES ZipCode(code, country)
+    id              INT          PRIMARY KEY,
+    zipCode         VARCHAR( 12)            ,   -- Postal code
+    country         VARCHAR(  2)            ,   -- Country code
+    streetName      VARCHAR(255) NOT NULL   ,   -- Street name
+    streetNumber    VARCHAR( 15) NOT NULL   ,   -- Street number (main door number, may not have number: s/n)
+    doorNumber      VARCHAR( 15)            ,   -- Door number if in an appartment block
+    personName      VARCHAR(255)            ,   -- Addresser or addressee if applicable
+    FOREIGN KEY (zipCode, country) REFERENCES ZipCode(code, country)
 );
 
 CREATE TABLE PostalService (
-    vat             varchar( 15)                ,
-    name            varchar(255) NOT NULL UNIQUE,           -- Name is a reserved keyword
-    headquarters    int                         ,
-    CONSTRAINT PK_PostalService PRIMARY KEY (vat),
-    CONSTRAINT FK_PostalService_PostOffice FOREIGN KEY (headquarters) REFERENCES PostOffice(idPostOffice)
+    vat             VARCHAR( 15) PRIMARY KEY,
+    name            VARCHAR(255) NOT NULL   ,   -- Name is a reserved keyword (?)
+    hq              INT          REFERENCES PostOffice(id)
 );
 
+CREATE TABLE PostOffice (
+    id              INT          PRIMARY KEY    ,
+    name            VARCHAR(255) NOT NULL UNIQUE,
+    address         INT          REFERENCES Address_(id),
+    postalService   VARCHAR( 15) REFERENCES PostalService(vat)
+);
+
+CREATE TABLE Vehicle (
+    plate       VARCHAR(15) PRIMARY KEY                         ,
+    postOffice  INT         REFERENCES PostOffice(id)           ,
+    maxWeight   FLOAT       CHECK (maxWeight > 0)               ,
+    type        VARCHAR(15) CHECK (type in ('motorbike', 'van'))
+);
+
+CREATE TABLE Person (
+    vat         VARCHAR( 15) PRIMARY KEY            ,
+    name        VARCHAR(255) NOT NULL               ,
+    address     INT          REFERENCES Address_(id),
+    phoneNumber VARCHAR( 31)                        
+);
+
+CREATE TABLE Client (
+    vat         VARCHAR(15) PRIMARY KEY REFERENCES Person
+);
+
+CREATE TABLE Employee (
+    vat         VARCHAR(15)     PRIMARY KEY REFERENCES Person   ,
+    postOffice  INT             REFERENCES PostOffice(id)       ,
+    salary      DECIMAL(38, 2)  NOT NULL CHECK (salary >= 0)
+);
+
+CREATE TABLE Manager (
+    vat         VARCHAR(15) PRIMARY KEY REFERENCES Employee
+);
+
+CREATE TABLE ShopKeeper (
+    vat         VARCHAR(15) PRIMARY KEY REFERENCES Employee,
+    supervisor  VARCHAR(15) REFERENCES Manager
+);
+
+CREATE TABLE Postman (
+    vat         VARCHAR(15) PRIMARY KEY REFERENCES Employee,
+    supervisor  VARCHAR(15) REFERENCES Manager
+);
