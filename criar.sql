b0e3cf2139a2e80c7c45e424880b7c2bbbcd1e44
Joao Sousa <joaosousa.pt@gmail.com>
2020-03-23 12:56:21 +0000

    Some progress (file criar.sql)

--- criar.sql
+++ criar.sql
@@ -1,6 +1,10 @@
 
 
--- TODO Check table declaration "order"
+-- TODO:
+-- Finish tables
+-- Figure out what to do with the postman->Postman type relations
+-- Check table declaration "order"
+
 DROP TABLE IF EXISTS ZipCode;
 DROP TABLE IF EXISTS Address_;      -- Address was highlighted but doesn't appear to be a reserved keyword
 DROP TABLE IF EXISTS PostalService;
@@ -20,4 +24,25 @@ DROP TABLE IF EXISTS BillItem;
 DROP TABLE IF EXISTS Price;
 
 
+CREATE TABLE ZipCode (
+    country varchar(255),
+    number_ int,                    -- Number is a reserved keyword
+    town varchar(255),
+    -- postman
+);
+
+CREATE TABLE Address_ (             -- Address
+    idAddress int,
+    -- zipNumber, country
+    streetName varchar(255),
+    streetNumber int,
+    doorNumber int,
+    personName varchar(255),        -- Person that lives at this address?
+);
+
+CREATE TABLE PostalService (
+    vat int,
+    name varchar(255),              -- Name is a reserved keyword
+    -- headquarters
+);
 
