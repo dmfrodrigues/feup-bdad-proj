6b6edc785d2a0ae897c0f498e1a5980a50d28b11
Joao Sousa <joaosousa.pt@gmail.com>
2020-03-23 18:01:42 +0000

    Update criar.sql

--- criar.sql
+++ criar.sql
@@ -3,10 +3,11 @@
 -- TODO:
 -- Finish tables
 -- Figure out what to do with the postman->Postman type relations
+-- Add Constrains
 -- Check table declaration "order"
 
 DROP TABLE IF EXISTS ZipCode;
-DROP TABLE IF EXISTS Address_;      -- Address was highlighted but doesn't appear to be a reserved keyword
+DROP TABLE IF EXISTS Address_;                  -- Address was highlighted but doesn't appear to be a reserved keyword
 DROP TABLE IF EXISTS PostalService;
 DROP TABLE IF EXISTS PostOffice;
 DROP TABLE IF EXISTS Vehicle;
@@ -17,7 +18,7 @@ DROP TABLE IF EXISTS Manager;
 DROP TABLE IF EXISTS ShopKeeper;
 DROP TABLE IF EXISTS Postman;
 DROP TABLE IF EXISTS Delivery;
-DROP TABLE IF EXISTS Order_;        -- Order is a reserved keyword
+DROP TABLE IF EXISTS Order_;                    -- Order is a reserved keyword
 DROP TABLE IF EXISTS Bill;
 DROP TABLE IF EXISTS CatalogItem;
 DROP TABLE IF EXISTS BillItem;
@@ -25,24 +26,80 @@ DROP TABLE IF EXISTS Price;
 
 
 CREATE TABLE ZipCode (
-    country varchar(255),
-    number_ int,                    -- Number is a reserved keyword
-    town varchar(255),
-    -- postman
+    country         varchar(255),
+    number_         int,                        -- Number is a reserved keyword
+    town            varchar(255),
+    postman     Postman,                        -- Is this correct?
+    PRIMARY KEY (country, number_)              -- Number is a reserved keyword
+
 );
 
-CREATE TABLE Address_ (             -- Address
-    idAddress int,
+CREATE TABLE Address_ (                         -- Address
+    idAddress       int PRIMARY KEY,
     -- zipNumber, country
-    streetName varchar(255),
-    streetNumber int,
-    doorNumber int,
-    personName varchar(255),        -- Person that lives at this address?
+    streetName      varchar(255),
+    streetNumber    int,
+    doorNumber      int,
+    personName      varchar(255),               -- Person that lives at this address?
 );
 
 CREATE TABLE PostalService (
-    vat int,
-    name varchar(255),              -- Name is a reserved keyword
+    vat             int PRIMARY KEY,
+    name_           varchar(255),               -- Name is a reserved keyword
     -- headquarters
 );
 
+CREATE TABLE PostalOffice (
+    idPostOffice    int PRIMARY KEY,
+    name_           varchar(255),               -- Name is a reserved keyword
+    -- address
+    -- postalService
+);
+
+CREATE TABLE Vehicle (
+    licensePlate    varchar(6) PRIMARY KEY,     -- 6 chars, right?
+    -- postOffice
+    maxWeight       int,
+    type_           varchar(255),               -- type is a reserved keyword
+);
+
+CREATE TABLE Person (
+    vat             int PRIMARY KEY,
+    name_           varchar(255),               -- Name is a reserved keyword
+    -- address
+    phoneNumber     int                         -- Any contraint here?
+);
+
+CREATE TABLE Client (
+    -- vat
+);
+
+CREATE TABLE Employee (
+    -- vat
+    -- postOffice
+    salary          int
+);
+
+CREATE TABLE Manager (
+    -- vat
+);
+
+CREATE TABLE ShopKeeper (
+    -- vat
+    -- supervisor
+);
+
+CREATE TABLE Postman (
+    -- vat
+    -- supervisor
+);
+
+CREATE TABLE Delivery (
+    idDelivery      int PRIMARY KEY,
+    -- from
+    -- to
+    -- registeredBy
+    -- order
+    timeRegister    time,
+    weight_         int,        
+);
