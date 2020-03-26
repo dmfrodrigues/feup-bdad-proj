82faf591ff57cbaedcf9f7e9eadd2f3581e169a4
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-03-26 15:40:04 +0000

    added more persons

--- create-povoar.sh
+++ create-povoar.sh
@@ -1,2 +1,3 @@
 rm -f povoar.sql
 python3 data/zip-codes/parse_zipcodes.py < data/zip-codes/todos_cp/todos_cp.csv >> povoar.sql
+cat data/persons/persons.sql >> povoar.sql
--- criar.sql
+++ criar.sql
@@ -1,11 +1,13 @@
-
-
 -- TODO:
 -- Check Constrains / consistency / references / foreign keys
 -- Check table declaration "order"
 
+-- SQLite keyworks: https://www.sqlite.org/lang_keywords.html
+
+PRAGMA foreign_keys=OFF;
+
 DROP TABLE IF EXISTS ZipCode;
-DROP TABLE IF EXISTS Address_;                  -- Address was highlighted but doesn't appear to be a reserved keyword
+DROP TABLE IF EXISTS Address;                  -- Address was highlighted but doesn't appear to be a reserved keyword
 DROP TABLE IF EXISTS PostalService;
 DROP TABLE IF EXISTS PostOffice;
 DROP TABLE IF EXISTS Vehicle;
@@ -17,7 +19,7 @@ DROP TABLE IF EXISTS ShopKeeper;
 DROP TABLE IF EXISTS Postman;
 DROP TABLE IF EXISTS Delivery;
 DROP TABLE IF EXISTS Category;
-DROP TABLE IF EXISTS Service_;
+DROP TABLE IF EXISTS Service;
 DROP TABLE IF EXISTS Order_;                    -- Order is a reserved keyword
 DROP TABLE IF EXISTS Bill;
 DROP TABLE IF EXISTS CatalogItem;
@@ -34,7 +36,7 @@ CREATE TABLE ZipCode (
     PRIMARY KEY (country, code)
 );
 
-CREATE TABLE Address_ (             -- Address
+CREATE TABLE Address (
     id              INT          PRIMARY KEY,
     zipCode         CHAR   ( 12)            ,   -- Postal code
     country         CHAR   (  2)            ,   -- Country code
@@ -47,28 +49,28 @@ CREATE TABLE Address_ (             -- Address
 
 CREATE TABLE PostalService (
     vat             CHAR   ( 15) PRIMARY KEY,
-    name_           VARCHAR(255) NOT NULL   ,   -- Name is a reserved keyword (?)
+    name            VARCHAR(255) NOT NULL   ,   -- Name is a reserved keyword (?)
     hq              INT          REFERENCES PostOffice(id)
 );
 
 CREATE TABLE PostOffice (
     id              INT          PRIMARY KEY    ,
-    name_           VARCHAR(255) NOT NULL UNIQUE,
-    address_        INT          REFERENCES Address_(id),
-    postalService   CHAR   ( 15) REFERENCES PostalService(vat)
+    name            VARCHAR(255) NOT NULL UNIQUE,
+    address         INT          REFERENCES Address(id),
+    postalService   CHAR   ( 15) NOT NULL REFERENCES PostalService(vat)
 );
 
 CREATE TABLE Vehicle (
     plate           CHAR(15)    PRIMARY KEY                         ,
     postOffice      INT         REFERENCES PostOffice(id)           ,
     maxWeight       FLOAT       CHECK (maxWeight > 0)               ,
-    type_           CHAR(15)    CHECK (type_ in ('motorbike', 'van'))
+    type           CHAR(15)    CHECK (type in ('motorbike', 'van'))
 );
 
 CREATE TABLE Person (
     vat             CHAR   ( 15) PRIMARY KEY            ,
-    name_           VARCHAR(255) NOT NULL               ,
-    address_        INT          REFERENCES Address_(id),
+    name            VARCHAR(255) NOT NULL               ,
+    address         INT          REFERENCES Address(id),
     phoneNumber     CHAR   ( 31)                        
 );
 
@@ -77,8 +79,8 @@ CREATE TABLE Client (
 );
 
 CREATE TABLE Employee (
-    vat             CHAR   (15)     PRIMARY KEY REFERENCES Person   ,
-    postOffice      INT             REFERENCES PostOffice(id)       ,
+    vat             CHAR   (15)     PRIMARY KEY REFERENCES Person       ,
+    postOffice      INT             NOT NULL REFERENCES PostOffice(id)  ,
     salary          DECIMAL(38, 2)  NOT NULL CHECK (salary >= 0)
 );
 
@@ -98,29 +100,29 @@ CREATE TABLE Postman (
 
 CREATE TABLE Delivery (
     id              INT         PRIMARY KEY                 ,
-    from_           INT         NULL REFERENCES Address_(id),
-    to_             INT         REFERENCES Address_(id)     ,
+    from_           INT         NULL REFERENCES Address(id),
+    to_             INT         REFERENCES Address(id)     ,
     registeredBy    CHAR(15)    REFERENCES ShopKeeper(vat)  ,
     order_          INT         NULL REFERENCES Order_(id)  ,
     timeRegister    TIMESTAMP   DEFAULT CURRENT_TIMESTAMP   ,
     weight          FLOAT       CHECK (weight > 0)          ,
-    service         VARCHAR(31) REFERENCES Service(name_)
+    service         VARCHAR(31) REFERENCES Service(name)
 );
 
 CREATE TABLE Category (
-    name_           VARCHAR(31) NOT NULL                    ,
+    name            VARCHAR(31) NOT NULL                    ,
     maxWeight       FLOAT       UNIQUE CHECK(maxWeight > 0)
 );
 
-CREATE TABLE Service_ (
-    name_           VARCHAR(31) NOT NULL
+CREATE TABLE Service (
+    name            VARCHAR(31) NOT NULL
 );
 
 CREATE TABLE Order_ (
     id          INT         PRIMARY KEY                                     ,
-    timeBegin       CHAR(19)                ,               -- "DD-MM-YYYY HH:MM:SS"
-    timeEnd         CHAR(19)                ,
-    type_       CHAR   (12) CHECK (type_ in ('generalOrder', 'lightOrder')) ,
+    timeBegin   CHAR(19)                ,               -- "DD-MM-YYYY HH:MM:SS"
+    timeEnd     CHAR(19)                ,
+    type        CHAR   (12) CHECK (type in ('generalOrder', 'lightOrder')) ,
     vehicle     VARCHAR(31) REFERENCES Vehicle(plate)                       ,
     postman     CHAR   (15) REFERENCES Postman(vat)
 );
@@ -137,7 +139,7 @@ CREATE TABLE Bill (
 
 CREATE TABLE CatalogItem (
     idCatalogItem   INT         PRIMARY KEY ,
-    description_    VARCHAR(255)            ,
+    description     VARCHAR(255)            ,
     price           INT
 );
 
@@ -149,7 +151,7 @@ CREATE TABLE BillItem (
     amount          INT     ,
     FOREIGN KEY (numBill, seller)   REFERENCES Bill(numBill, seller)  ,
     FOREIGN KEY (catalogItem)       REFERENCES CatalogItem(idCatalogItem) ,
-    PRIMARY KEY     (numBill, seller, catalogItem)
+    PRIMARY KEY (numBill, seller, catalogItem)
 );
 
 CREATE TABLE Price (
