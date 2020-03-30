adcd3b388f10824d57eeafcb30b8e7ab6ec60d40
Joao Sousa <joaosousa.pt@gmail.com>
2020-03-30 16:22:08 +0100

    Added ON UPDATE CASCADE on all the REFERENCES
    Check if it is done correctly on the FOREIGN KEYs.

--- criar.sql
+++ criar.sql
@@ -29,10 +29,10 @@ DROP TABLE IF EXISTS Price;
 PRAGMA foreign_keys=ON;
 
 CREATE TABLE ZipCode (
-    country CHAR   ( 2)                         ,   -- ISO 3166-1 alpha-2 country code (PT for Portugal)
-    code    CHAR   (12)                         ,   -- Postal code (alphanumeric and dashes)
-    town    VARCHAR(63) NOT NULL                ,   -- Town name
-    postman CHAR   (15) REFERENCES Postman(vat) ,   -- Postman VAT number
+    country CHAR   ( 2)                         ,                   -- ISO 3166-1 alpha-2 country code (PT for Portugal)
+    code    CHAR   (12)                         ,                   -- Postal code (alphanumeric and dashes)
+    town    VARCHAR(63) NOT NULL                ,                   -- Town name
+    postman CHAR   (15) REFERENCES Postman(vat) ON UPDATE CASCADE,  -- Postman VAT number
     PRIMARY KEY (country, code)
 );
 
@@ -44,25 +44,25 @@ CREATE TABLE Address (
     streetNumber    VARCHAR( 15) NOT NULL   ,   -- Street number (main door number, may not have number: s/n)
     doorNumber      VARCHAR( 15) NULL       ,   -- Door number if in an appartment block
     personName      VARCHAR(255) NULL       ,   -- Addresser or addressee if applicable
-    FOREIGN KEY (zipCode, country) REFERENCES ZipCode(code, country)
+    FOREIGN KEY (zipCode, country) REFERENCES ZipCode(code, country) ON UPDATE CASCADE
 );
 
 CREATE TABLE PostalService (
     vat             CHAR   ( 15) PRIMARY KEY,
     name            VARCHAR(255) NOT NULL   ,   -- Name is a reserved keyword (?)
-    hq              INT          REFERENCES PostOffice(id)
+    hq              INT          REFERENCES PostOffice(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE PostOffice (
     id              INT          PRIMARY KEY    ,
     name            VARCHAR(255) NOT NULL UNIQUE,
-    address         INT          NOT NULL REFERENCES Address(id),
-    postalService   CHAR   ( 15) NOT NULL REFERENCES PostalService(vat)
+    address         INT          NOT NULL REFERENCES Address(id) ON UPDATE CASCADE,
+    postalService   CHAR   ( 15) NOT NULL REFERENCES PostalService(vat) ON UPDATE CASCADE
 );
 
 CREATE TABLE Vehicle (
     plate           CHAR(15)    PRIMARY KEY                         ,
-    postOffice      INT         NOT NULL REFERENCES PostOffice(id)  ,
+    postOffice      INT         NOT NULL REFERENCES PostOffice(id)  ON UPDATE CASCADE,
     maxWeight       FLOAT       CHECK (maxWeight > 0)               ,
     type           CHAR(15)     CHECK (type in ('motorbike', 'van'))
 );
@@ -70,43 +70,43 @@ CREATE TABLE Vehicle (
 CREATE TABLE Person (
     vat             CHAR   ( 15) PRIMARY KEY            ,
     name            VARCHAR(255) NOT NULL               ,
-    address         INT          NOT NULL REFERENCES Address(id),
+    address         INT          NOT NULL REFERENCES Address(id)  ON UPDATE CASCADE,
     phoneNumber     CHAR   ( 31)                        
 );
 
 CREATE TABLE Client (
-    vat             CHAR(15) PRIMARY KEY REFERENCES Person
+    vat             CHAR(15) PRIMARY KEY REFERENCES Person ON UPDATE CASCADE
 );
 
 CREATE TABLE Employee (
-    vat             CHAR   (15)     PRIMARY KEY REFERENCES Person       ,
-    postOffice      INT             NOT NULL REFERENCES PostOffice(id)  ,
+    vat             CHAR   (15)     PRIMARY KEY REFERENCES Person ON UPDATE CASCADE     ,
+    postOffice      INT             NOT NULL REFERENCES PostOffice(id) ON UPDATE CASCADE,
     salary          DECIMAL(38, 2)  NOT NULL CHECK (salary >= 0)
 );
 
 CREATE TABLE Manager (
-    vat             CHAR(15) PRIMARY KEY REFERENCES Employee
+    vat             CHAR(15) PRIMARY KEY REFERENCES Employee ON UPDATE CASCADE
 );
 
 CREATE TABLE ShopKeeper (
-    vat             CHAR(15) PRIMARY KEY REFERENCES Employee,
-    supervisor      CHAR(15) NOT NULL REFERENCES Manager
+    vat             CHAR(15) PRIMARY KEY REFERENCES Employee ON UPDATE CASCADE,
+    supervisor      CHAR(15) NOT NULL REFERENCES Manager ON UPDATE CASCADE
 );
 
 CREATE TABLE Postman (
-    vat             CHAR(15) PRIMARY KEY REFERENCES Employee,
-    supervisor      CHAR(15) NOT NULL REFERENCES Manager
+    vat             CHAR(15) PRIMARY KEY REFERENCES Employee ON UPDATE CASCADE,
+    supervisor      CHAR(15) NOT NULL REFERENCES Manager ON UPDATE CASCADE
 );
 
 CREATE TABLE Delivery (
     id              INT         PRIMARY KEY                 ,
-    from_           INT         NULL REFERENCES Address(id),
-    to_             INT         NOT NULL REFERENCES Address(id)     ,
-    registeredBy    CHAR(15)    NOT NULL REFERENCES ShopKeeper(vat)  ,
-    order_          INT         NULL REFERENCES Order_(id)  ,
+    from_           INT         NULL REFERENCES Address(id) ON UPDATE CASCADE           ,
+    to_             INT         NOT NULL REFERENCES Address(id) ON UPDATE CASCADE       ,
+    registeredBy    CHAR(15)    NOT NULL REFERENCES ShopKeeper(vat) ON UPDATE CASCADE   ,
+    order_          INT         NULL REFERENCES Order_(id) ON UPDATE CASCADE            ,
     timeRegister    TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP   ,
     weight          FLOAT       CHECK (weight > 0)          ,
-    service         VARCHAR(31) NOT NULL REFERENCES Service(name)
+    service         VARCHAR(31) NOT NULL REFERENCES Service(name) ON UPDATE CASCADE
 );
 
 CREATE TABLE Category (
@@ -119,29 +119,29 @@ CREATE TABLE Service (
 );
 
 CREATE TABLE Price (
-    category        VARCHAR(31)     NOT NULL REFERENCES Category(name)   ,
-    service         VARCHAR(31)     NOT NULL REFERENCES Service(name)    ,
+    category        VARCHAR(31)     NOT NULL REFERENCES Category(name) ON UPDATE CASCADE,
+    service         VARCHAR(31)     NOT NULL REFERENCES Service(name) ON UPDATE CASCADE ,
     price           DECIMAL(12, 2)                              ,
     PRIMARY KEY     (category, service)
 );
 
 CREATE TABLE Order_ (
-    id          INT         PRIMARY KEY                                     ,
+    id          INT         PRIMARY KEY ,
     timeBegin   DATETIME                ,               -- "DD-MM-YYYY HH:MM:SS"
     timeEnd     DATETIME                ,
-    type        CHAR   (12) CHECK (type in ('generalOrder', 'lightOrder')) ,
-    vehicle     VARCHAR(31) NULL REFERENCES Vehicle(plate)                       ,
-    postman     CHAR   (15) NOT NULL REFERENCES Postman(vat),
+    type        CHAR   (12) CHECK (type in ('generalOrder', 'lightOrder'))      ,
+    vehicle     VARCHAR(31) NULL REFERENCES Vehicle(plate) ON UPDATE CASCADE    ,
+    postman     CHAR   (15) NOT NULL REFERENCES Postman(vat) ON UPDATE CASCADE  ,
     CHECK (timeBegin < timeEnd)
 );
 
 CREATE TABLE Bill (
     numBill         INT                                                  ,
-    seller          CHAR(15)    REFERENCES PostalService(vat)            ,
+    seller          CHAR(15)    REFERENCES PostalService(vat) ON UPDATE CASCADE         ,
     timeIssue       TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP       ,
     price           DECIMAL(12, 2) NOT NULL                              ,
-    consumer        CHAR(15)    NOT NULL REFERENCES Client(vat)          ,
-    issuer          CHAR(15)    NOT NULL REFERENCES ShopKeeper(vat)      ,
+    consumer        CHAR(15)    NOT NULL REFERENCES Client(vat) ON UPDATE CASCADE       ,
+    issuer          CHAR(15)    NOT NULL REFERENCES ShopKeeper(vat) ON UPDATE CASCADE   ,
     PRIMARY KEY     (numBill, seller)
 );
 
@@ -154,9 +154,9 @@ CREATE TABLE CatalogItem (
 CREATE TABLE BillItem (
     numBill         INT             NOT NULL,
     seller          CHAR(15)        NOT NULL,
-    catalogItem     INT             NOT NULL REFERENCES CatalogItem(idCatalogItem),
+    catalogItem     INT             NOT NULL REFERENCES CatalogItem(idCatalogItem) ON UPDATE CASCADE,
     priceThen       DECIMAL(12, 2)  ,
     amount          INT             CHECK(amount > 0),
     PRIMARY KEY (numBill, seller, catalogItem),
-    FOREIGN KEY (numBill, seller)   REFERENCES Bill(numBill, seller)
+    FOREIGN KEY (numBill, seller)   REFERENCES Bill(numBill, seller) ON UPDATE CASCADE
 );
