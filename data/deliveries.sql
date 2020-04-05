9eb7570982187e7bc3b1b89f431ec9f6cda9e919
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-04-05 17:49:42 +0100

    Revert "Fixed deliveries"
    This reverts commit 2d984e84f6ee6f543434c59e096d2aedd87e917a.

--- data/deliveries.sql
+++ data/deliveries.sql
@@ -1,15 +1,6 @@
-INSERT INTO Address(id, zipCode, country, streetName, streetNumber, personName) VALUES 
-(7, "4495-447", "PT", "Cruzeiro das Avestruzes", "63", "Fernando Correia Marques"),
-(8, "4620-022", "PT", "Avenida dos Enterrados", "99", "Fátima Lopes"); 
-INSERT INTO Delivery(id, from_, to_, registeredBy, order_, weight, service) VALUES (1, 7, 8, "643378204", 2, 0.100, "Economy");
+INSERT INTO Delivery(id, from_, to_, registeredBy, order_, weight, service) VALUES
+(1, 1, 2, "643378204", 2, 0.100, "Economy"),
+(3, 2, 1, "643378204", 1, 12.500, "Express");
 
-INSERT INTO Address(id, zipCode, country, streetName, streetNumber, personName) VALUES 
-(9, "4620-200", "PT", "Alameda Alagada", "27", "José Condessa"),
-(10, "4620-201", "PT", "Trilho Torto", "81", "Miguel Góis");
-INSERT INTO Delivery(id, from_, to_, registeredBy, order_, weight, service) VALUES (3, 10, 9, "643378204", 1, 12.500, "Express");
-
-INSERT INTO Address(id, zipCode, country, streetName, streetNumber, personName) VALUES 
-(11, "4785-095", "PT", "Caminho do Camilo", "12", "António Salazar"),
-(12, "9400-025", "PT", "Rua do Doutor", "80", "Marquês de Pombal");
 INSERT INTO Delivery(id, from_, to_, registeredBy, order_, weight, service, numBill, seller) VALUES
-(2, 12, 11, "643378204", 2, 0.300, "Registered", 1894, "500077568");
+(2, 1, 2, "643378204", 2, 0.300, "Registered", 1894, "500077568");
