bad82e842f826e141213dec8ad10e9c9f22d5343
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-04-05 19:35:12 +0100

    Bills and catalog

--- data/bills.sql
+++ data/bills.sql
@@ -10,9 +10,20 @@ INSERT INTO BillItem(numBill, seller, catalogItem, priceThen, amount) VALUES
 (1894, "500077568", 2, 0.30, 2);
 UPDATE Bill SET price=0.60 WHERE numBill=1894 AND seller="500077568";
 
-
 INSERT INTO Bill(numBill, seller, price, consumer, issuer) VALUES
 (4498, "500077568", 0.00, "123456789", 	"643378204");
 INSERT INTO BillItem(numBill, seller, catalogItem, priceThen, amount) VALUES
 (4498, "500077568", 3, 1.05, 5);
 UPDATE Bill SET price=5.25 WHERE numBill=4498 AND seller="500077568";
+
+INSERT INTO Bill(numBill, seller, price, consumer, issuer) VALUES
+(8743, "500077568", 0.00, "123456789", "987264567");
+INSERT INTO BillItem(numBill, seller, catalogItem, priceThen, amount) VALUES
+(8743, "500077568", 4, 0.50, 5);
+UPDATE Bill SET price=2.50 WHERE numBill=8743 AND seller="500077568";
+
+INSERT INTO Bill(numBill, seller, price, consumer, issuer) VALUES
+(9922, "500077568", 0.00, "2211998877", "223456483");
+INSERT INTO BillItem(numBill, seller, catalogItem, priceThen, amount) VALUES
+(9922, "500077568", 5, 0.99, 1);
+UPDATE Bill SET price=0.99 WHERE numBill=9922 AND seller="500077568";
