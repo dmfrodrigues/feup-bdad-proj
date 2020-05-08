f7fb3d9229e59c63fb3b9b77a919576ec0f5856d
Joao Sousa <joaosousa.pt@gmail.com>
2020-05-08 22:01:31 +0100

    Added 5th query out of 5.

---
+++ queries/int7.sql
@@ -0,0 +1,5 @@
+.mode	columns
+.headers	on
+.nullvalue	NULL
+
+SELECT Person.name as Postman_Name, count(*) as Number_of_Deliveries FROM Order_, Postman, Person WHERE Postman.vat=Person.vat AND Order_.timeEnd like '%2020-07-%' AND Order_.postman=Postman.vat GROUP BY Person.name ORDER BY count(*) DESC;
