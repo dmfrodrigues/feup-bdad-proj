9094fcd2687d8a3292865ce2a86f3044b6c416f3
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-05-09 17:00:58 +0100

    Small tweaks

--- queries/int1.sql
+++ queries/int1.sql
@@ -2,4 +2,4 @@
 .headers	on
 .nullvalue	NULL
 
-SELECT name, date(timeBegin), (julianday(timeEnd) - julianday(timeBegin)) * 24 FROM Person INNER JOIN Order_ ON Person.vat=Order_.postman ORDER BY name;
+SELECT name, date(timeBegin) as date, (julianday(timeEnd) - julianday(timeBegin)) * 24 AS OrderDuration FROM Person INNER JOIN Order_ ON Person.vat=Order_.postman ORDER BY name;
